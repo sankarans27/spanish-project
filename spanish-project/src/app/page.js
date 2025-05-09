3315d5f4227a4827a0bbd32cf922dda89d456dc0
SiaSankaran <sankarans27@greenhill.org>
2025-05-09 18:53:00 -0500

    will it work?

---
+++ spanish-project/src/app/page.js
@@ -0,0 +1,16 @@
+'use client'
+
+export default function Home() {
+    return (
+        <div>
+            <div className="topbar">
+                <a>Espana</a>
+                <a>Colombia</a>
+                <a>Cuba</a>
+                <a>Argentina</a>
+            </div>
+            <h1>La Musica y Revolucion</h1>
+            <p>Very skibidi music stuff</p>
+        </div>
+    )
+}
